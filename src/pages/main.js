f927c9655bfd011c2dfe2967f13be03bd8da39f1
Esau R <mumbonumber7@gmail.com>
2023-04-07 04:02:04 -0500

    Added hiding navbar on scroll

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,22 +1,44 @@
+import { Content } from "next/font/google";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
 export default function Main() {
     const [clicked, setClicked] = useState(false);
+
+    const [hState, sethState] = useState("top");
+
+    useEffect(() => {
+        var lastVal = 0;
+        window.onscroll = function () {
+        let y = window.scrollY;
+        if (y > lastVal) {
+            sethState("down");
+        }
+        if (y < lastVal) {
+            sethState("up");
+        }
+        if (y === 0) {
+            sethState("top");
+        }
+        lastVal = y;
+        };
+    }, []);
+
+
     return (
         
         
         <div className={clicked ? "dark-mode" : "body"}>
             
-            <div className="headbar">
+            <div className={"headbar" + hState} >
                     <a href="#aboutme" className="nav" >About Me</a><br/>
                     <a href="#contactme" className="nav">Contact</a><br/>
-                    <a href="/resume" className="nav">Resume</a><br/>
+                    <a href="#resume" className="nav">Resume</a><br/>
                     <a href="#" className="nav">Projects</a> 
             </div>
 
-            <span className="circle">
+            <span className={clicked ? "circle2":"circle"}>
             </span>
-
 
             <div className="intro">
                 <h1 className="headline">
@@ -26,12 +48,12 @@ export default function Main() {
                     <span className="U">u</span>,<br/> 
                     A Software Developer.
                 </h1><br/>
+                
                 <h2 className="subheading">Passionate Developer based in Canada</h2><br/>
 
                 <a href="https://github.com/codehoover" target="_blank" className="git"><Image src={clicked ?  "/github_white.png" : "/github.png"} width={40} height={40}/></a>
                 <a href="https://www.linkedin.com/in/esau-rahim-b7905b139/" target="_blank" className="linkedin"><Image src={clicked ? "/linkedin_white.png" : "/linkedin.png"} width={40} height={40}/></a>
             </div>
-
 
             <div className="arrow">
                 <span></span>
@@ -45,16 +67,41 @@ export default function Main() {
                 height={25}/>
             </button>
 
-            <div className="aboutme" id="aboutme">
+            <div id="aboutme">
+
                 <h1 className="headline">About Me</h1>
                 <h2>Dedicated developer based in Canada</h2>
                 <p>
-                Computer Science Graduate (BsC. Comp Sci) from Toronto Metropolitan University
-                (formerly Ryerson University). Team player with a results oriented approach 
-                with a demonstrated history in the Information Technology Industry. 
-                </p><br/>
-                <a href="/Resume_EsauRahim.pdf" className="cv" download>Downloadable CV</a>
+                    Hi, welcome to my portfolio! My name is Esau and I'm a FullStack Developer located in Canada. Computer Science Graduate (BsC. Comp Sci) from Toronto Metropolitan University
+                    (formerly Ryerson University).<br/><br/>
 
+                    Sports enthusiast and avid supporter of the Toronto Raptors.
+                    I have a history of competing and excelling in both individual as well as team based athletics (see awards). <br/>
+                    Well organized, problem solver with a results oriented approach.<br/><br/>
+
+                    Here are a few languages and technologies I use:
+
+                </p><br/>
+                <div className="stack">
+                    <ul>
+                        <li>Reactjs</li>
+                        <li>Nodejs</li>
+                        <li>Expressjs</li>
+                        <li>MySQL</li>
+                        <li>PostgreSQL</li>
+                        <li>Python</li>
+                        <li>Javascript/HTML/CSS</li>
+                        <li>Java</li>
+                        <li>TypeScript</li>
+                    </ul>
+                </div>
+            </div>
+
+
+            <div id="resume">
+                <h1>Skills and Experience</h1>
+                <span className="card"></span>
+                <a href="/Resume_EsauRahim.pdf" className="cv" download>Downloadable CV</a>
             </div>
             
             <div id="contactme">
@@ -62,8 +109,6 @@ export default function Main() {
                     <h1 style={{display:'inline-block'}}>Contact Me</h1>
                     <Image className="team_icon" src="/team.png" width={50} height={50}/>
                 </div>
-                
-                
                 
                 <p>I'm interested in freelance opportunities and connecting with likeminded individuals.
                     If you have an questions regarding my portfolio or want to connect feel free to reach me below!
@@ -73,8 +118,8 @@ export default function Main() {
                     <input className="name" placeholder="Name"></input>
                     <input className="email" placeholder="Email"></input>
                     <input className="subject" placeholder="Subject"></input>
-                    <input className="message" placeholder="Message"></input>
-                    <button>Send!</button>
+                    <textarea className="message" placeholder="Message"></textarea><br/>
+                    <button className="form_sub">S e n d !</button>
                 </form>
             </div>
 
