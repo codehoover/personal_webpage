10d25965fc024504e8a911f9125e20f3e071f2bd
Esau R <mumbonumber7@gmail.com>
2023-04-09 02:49:51 -0500

    added more sizing functionality

--- src/pages/main.js
+++ src/pages/main.js
@@ -33,7 +33,7 @@ export default function Main() {
             <div className={"headbar" + hState} >
                     <a href="#aboutme" className="nav" >About Me</a><br/>
                     <a href="#" className="nav">Projects</a>
-                    <a href="#resume" className="nav">Resume</a><br/> 
+                    <a href="#resume" className="nav">Work</a><br/> 
                     <a href="#contactme" className="nav">Contact</a><br/>
             </div>
 
@@ -47,13 +47,18 @@ export default function Main() {
                     <span className="A">a</span>
                     <span className="U">u</span>,<br/> 
                     A Software Developer.
+
+                    <div className="circle_anim">
+                    </div>
+                    
                 </h1><br/>
                 
                 <h2 className="subheading">Passionate Developer based in Canada</h2><br/>
 
-                <a href="https://github.com/codehoover" target="_blank" className="git"><Image src={clicked ?  "/github_white.png" : "/github.png"} width={40} height={40}/></a>
-                <a href="https://www.linkedin.com/in/esau-rahim-b7905b139/" target="_blank" className="linkedin"><Image src={clicked ? "/linkedin_white.png" : "/linkedin.png"} width={40} height={40}/></a>
-                <Image className="img1"src="/code_comp.png" width={100} height={100} alt="Computer Clip art"/>
+                <a href="https://github.com/codehoover" target="_blank" className="git"><Image src={clicked ?  "/github_white.png" : "/github.png"} width={40} height={40} alt="github link"/></a>
+                <a href="https://www.linkedin.com/in/esau-rahim-b7905b139/" target="_blank" className="linkedin"><Image src={clicked ? "/linkedin_white.png" : "/linkedin.png"} width={40} height={40} alt="linkedin profile link"/></a>
+                
+
             </div>
 
             <div className="arrow">
@@ -65,7 +70,8 @@ export default function Main() {
             <button className="toggle" onClick={()=> setClicked(current=> !current)}>
                 <Image src={clicked ? "/light_mode.png" : "/dark_mode.png"}
                 width={25}
-                height={25}/>
+                height={25}
+                alt="light/dark mode toggle"/>
             </button>
 
             <div id="aboutme">
@@ -80,17 +86,29 @@ export default function Main() {
                 <div className="aboutme_body">
                     <h2>Dedicated developer based in Canada</h2>
                     <p>
-                        Hi, welcome to my portfolio! My name is Esau and I'm a FullStack Developer located in Canada. Computer Science Graduate (BsC. Comp Sci) from Toronto Metropolitan University
-                        (formerly Ryerson University).<br/><br/>
+                        Hi, welcome to my portfolio! My name is Esau and I'm a FullStack Developer located in Canada. I recently graduated from Toronto Metropolitan University (formerly Ryerson University) Computer Science Graduate (BsC. Comp Sci).<br/><br/>
 
-                        Sports enthusiast and avid supporter of the Toronto Raptors.
-                        I have a history of competing and excelling in both individual as well as team based athletics (see awards). <br/>
                         Well organized, problem solver with a results oriented approach.<br/><br/>
 
-                        Here are a few languages and technologies I use:
-
+                        Outside of programming I am a movie connoisseur, sports enthusiast and avid supporter of the Toronto Raptors.
+                        If you would like to see my past awards in athletics as well as academics please see <a href="#">Awards</a>. <br/>
+                        <br/><br/>
                     </p><br/>
-                    <div>
+
+                </div>
+                
+            </div>
+
+
+            <div id="resume">
+                <h1 className="headline"><span className="blue">S</span>kill<span className="blue">s</span> <span className="green">a</span>nd <span className="red">E</span>xp<span className="blue">e</span>ri<span className="green">e</span>nc<span className="yellow">e</span></h1>
+
+                <div>
+                    <Image className="card" src="/nellie.jpg" width={100} height={100} alt="nellie"/>
+                    <Image className="card" src="/Tmu.jpg" width={100} height={100} alt="tmu"/>
+                </div>
+                Here are a few languages and technologies I use:
+                <div>
                         <ul className="stack">
                             <li>Reactjs</li>
                             <li>Nodejs</li>
@@ -104,33 +122,20 @@ export default function Main() {
                         </ul>
                     </div>
 
-                </div>
-                
-            </div>
-
-
-            <div id="resume">
-                <h1 className="headline">Skills and Experience</h1>
-
-                <div>
-                    <Image className="card" src="/nellie.jpg" width={100} height={100} alt="nellie"/>
-                    <Image className="card" src="/Tmu.jpg" width={100} height={100} alt="tmu"/>
-                </div>
-
 
                 
-                <p>Grab a copy of my cv!</p>
-                <a href="/Resume_EsauRahim.pdf" className="cv" download>Downloadable CV</a>
+                <p style={{paddingBottom:"15px"}}>Grab a copy of my cv!</p>
+                <a href="/Resume_EsauRahim.pdf" className={clicked ? "form_sub_dark":"form_sub"} download>Downloadable CV</a>
             </div>
             
             <div id="contactme">
                 <div className="contactme_header">
                     <h1 className="headline">Cont<span className="green">a</span>ct M<span className="red">e</span></h1>
-                    <Image className="team_icon" src="/team.png" width={70} height={70}/>
+                    <Image className="team_icon" src="/team.png" width={70} height={70} alt="team icon"/>
                 </div>
                 
                 <p>I'm interested in freelance opportunities and connecting with likeminded individuals.
-                    If you have an questions regarding my portfolio or want to connect feel free to reach me below!
+                    If you have any questions regarding my portfolio or want to connect feel free to reach me below!
                 </p>
 
                 <form className="fillable">
@@ -144,8 +149,13 @@ export default function Main() {
 
             <div className="location">
                 <p>Esau Rahim</p>
-                <p>Remote, Canada</p> <br/>
+                <p>Location 1: Winnipeg, Manitoba, Canada</p>
+                <p>Location 2: Toronto, Ontario, Canada</p> <br/>
                 <p>email: esaurahim@gmail.com</p>
+            </div>
+
+            <div className="footer">
+                <p>Designed & Built by Esau Rahim</p>
             </div>
 
 
