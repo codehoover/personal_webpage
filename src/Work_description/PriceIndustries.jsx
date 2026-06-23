69d5273d4446c6ace6dc51c72639d76f9fd28612
Esau R <mumbonumber7@gmail.com>
2026-06-22 21:41:46 -0500

    added new work experience at price industries

---
+++ src/Work_description/PriceIndustries.jsx
@@ -0,0 +1,13 @@
+const PriceIndustries = () =>{
+    return(
+        <div className="testing_div">
+            <h2> Software Developer @ Price Industries</h2>
+            <h4>June 2026 - Present</h4>
+            <ul>
+                <li> Developing Price Select - an enterprise CPQ (configure, price, quote)-style platform that allows engineers, contractors, and sales teams to configure, select and order engineered HVAC and environmental control products based on custom technical constraints.</li>
+            </ul>
+        </div>  
+    )
+}
+
+export default PriceIndustries;
